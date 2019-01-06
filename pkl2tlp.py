e2376dcfc6df72e3427edfd2cc945282122a903b
leoguignard <31226251+leoguignard@users.noreply.github.com>
2019-01-06 13:36:16 +0100

    properties handling and name ordering

--- pkl2tlp.py
+++ pkl2tlp.py
@@ -2,8 +2,25 @@
 
 import argparse
 import cPickle as pkl
+import numpy as np
 
-def write_tlp_from_lin_tree(name, lin_tree_information, lin_name, ListProperties=[]):
+def format_names(names_which_matter):
+    """
+    Return an ensured formated cell names
+    """
+    tmp={}
+    for k, v in names_which_matter.iteritems():
+        try:
+            val=v.split('.')[1][:-1]
+            tmp[k] = (v.split('.')[0][0] + '%02d'%int(v.split('.')[0][1:]) + '.'
+                      + '%04d'%int(v.split('.')[1][:-1]) + v.split('.')[1][-1])
+        except Exception as e:
+            print v
+            exit()
+    return tmp
+
+
+def write_tlp_from_lin_tree(name, lin_tree_information, lin_name):
     """
     Write a lineage tree into an understable tulip file
     name : path to the tulip file to create
@@ -12,24 +29,32 @@ def write_tlp_from_lin_tree(name, lin_tree_information, lin_name, ListProperties
     """
     
     lin_tree=lin_tree_information[lin_name]
-    if ListProperties is None:
-        ListProperties=['volumes_information','h_mins_information','sigmas_information']
-    properties=[] #Properties Dictionary initialisation
-    for l in ListProperties:
-        if l is not 'Names':
-            properties.append((l,lin_tree_information[l],sum(lin_tree_information[l].values())/float(len(lin_tree_information[l]))))
-
 
     nodes=set(lin_tree.keys()).union(set([v for values in lin_tree.values() for v in values]))
 
-    f=open(name, "w")
     inv_lin_tree={v:k for k, vals in lin_tree.iteritems() for v in vals}
+    do_names = 'Names' in lin_tree_information
+    if do_names:
+        names = lin_tree_information['Names']
+        names_which_matter = { k : v for k, v in names.iteritems() if v!='' and v!='NO' and k in nodes}
+        names_formated = format_names(names_which_matter)
+        order_on_nodes = np.array(names_formated.keys())[np.argsort(names_formated.values())]
+        nodes.difference_update(set(order_on_nodes))
+        tmp_names = {}
+        for k, v in lin_tree_information['Names'].iteritems():
+            if len(lin_tree.get(inv_lin_tree.get(k, -1), [])) != 1:
+                tmp_names[k] = v
+        lin_tree_information['Names'] = tmp_names
+
+    f = open(name, "w")
     f.write("(tlp \"2.0\"\n")
     f.write("(nodes ")
+    if do_names:
+        for n in order_on_nodes:
+           f.write(str(n)+ " ")
     for n in nodes:
         f.write(str(n)+ " ")
     f.write(")\n")
-
 
     nodes=set(lin_tree.keys()).union(set([v for values in lin_tree.values() for v in values]))
     count_edges=0
@@ -43,15 +68,33 @@ def write_tlp_from_lin_tree(name, lin_tree_information, lin_name, ListProperties
         f.write("\t(node " + str(node) + str(" \"") + str(node) + "\")\n")
     f.write(")\n")
 
-    for property in properties:
-        prop_name=property[0]
-        vals=property[1]
-        default=property[2]
-        f.write("(property 0 string \""+prop_name+"\"\n")
-        f.write("\t(default \""+str(default)+"\" \"0\")\n")
-        for node in nodes:
-            f.write("\t(node " + str(node) + str(" \"") + str(vals.get(node, default)) + "\")\n")
-        f.write(")\n") 
+    for prop_name, property in lin_tree_information.iteritems():
+        vals=property
+        if prop_name != lin_name and type(vals.values()[0]) != dict:
+            if type(vals.values()[0]) == str:
+                default=''
+                f.write("(property 0 string \""+prop_name+"\"\n")
+                f.write("\t(default \""+str(default)+"\" \"0\")\n")
+                for node in nodes:
+                    f.write("\t(node " + str(node) + str(" \"") + str(vals.get(node, default)) + "\")\n")
+                f.write(")\n") 
+                if 'Fate' in prop_name or 'fate' in prop_name:
+                    fates = set(vals.values())
+                    corres = dict(zip(fates, range(len(fates))))
+                    corres[''] = len(fates) + 1
+                    f.write("(property 0 double \""+prop_name+"_int\"\n")
+                    f.write("\t(default \""+str(corres[default])+"\" \"0\")\n")
+                    for node in nodes:
+                        f.write("\t(node " + str(node) + str(" \"") + str(corres[vals.get(node, default)]) + "\")\n")
+                    f.write(")\n") 
+
+            else:
+                default=np.median(vals.values())
+                f.write("(property 0 double \""+prop_name+"\"\n")
+                f.write("\t(default \""+str(default)+"\" \"0\")\n")
+                for node in nodes:
+                    f.write("\t(node " + str(node) + str(" \"") + str(vals.get(node, default)) + "\")\n")
+                f.write(")\n") 
     f.write(")")
     f.close()
 
@@ -71,7 +114,7 @@ def main():
         lin_name = 'Lineage tree'
     else:
         print "There is no known key value for the lineage tree (expected 'lin_tree' or 'Lineage tree').\n\n\tThe script will not output a tulip file."
-        exit
+        exit()
     write_tlp_from_lin_tree(args.output, DATA, lin_name)
 
 
